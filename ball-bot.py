743ecb150fccd1a305eabf875f7f84b1249cd672
c-string <str@cock.li>
2017-02-07 16:19:31 -0600

    Add files via upload
    Initial commit

---
+++ ball-bot.py
@@ -0,0 +1,49 @@
+import discord
+import asyncio
+
+bot = discord.Client()
+
+@bot.event
+@asyncio.coroutine
+# both the above lines are need for python 3.4.2
+# this would be different for newer versions of python
+def on_ready():
+
+	# event for when the bot is is done preparing the data received from discord
+	# prints out the name and id of the bot to the termainal
+	# and makes the bot send a ready message to the discord text channel
+	print('Login succesful')
+	print("Bot username: %s" % bot.user.name)
+	print("Bot id: %s" % bot.user.id)
+	print('------')
+	channel = discord.Object(id='278559327789514753')
+	yield from bot.send_message(channel,'Ohayou!')
+
+@bot.event
+@asyncio.coroutine
+def on_message(message):
+
+	# prevents the bot from responding to itself
+	if message.author == bot.user:
+		return
+	
+	if (message.content == 'ping'):
+		if (message.author.id == '112361377179398144'):
+			# BEEEEEES message
+			msg = '{0.author.mention} gnop!'.format(message)
+			yield from bot.send_message(message.channel, msg)
+		else:
+			# other people
+			msg = '{0.author.mention} pong!'.format(message)
+			yield from bot.send_message(message.channel, msg)
+
+
+	# kill command
+	# it should only accept this command from my account
+	if (message.content == '!kill' and  message.author.id == '232684662244376576'):
+		yield from bot.send_message(message.channel, 'Oyasumi!')
+		yield from bot.close()
+		exit()
+
+# bot token, do not change this
+bot.run("Mjc4NTQ5NDY2MTc5NDM2NTQ1.C3uBMQ.fr_cityxYfj93qTNb2K3JgFkkEw")
