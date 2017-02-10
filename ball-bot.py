eeeab897e0aaddaefc6be638d08d90898ae390f7
c-string <str@cock.li>
2017-02-10 11:09:59 -0600

    Add files via upload
    Added random_message()
    Added main()
    Added response for a specific substring
    Added some """helpful""" comments
    General code refactoring

--- ball-bot.py
+++ ball-bot.py
@@ -1,5 +1,6 @@
 import discord
 import asyncio
+from random import randint
 
 bot = discord.Client()
 
@@ -9,41 +10,58 @@ bot = discord.Client()
 # this would be different for newer versions of python
 def on_ready():
 
-	# event for when the bot is is done preparing the data received from discord
-	# prints out the name and id of the bot to the termainal
-	# and makes the bot send a ready message to the discord text channel
-	print('Login succesful')
-	print("Bot username: %s" % bot.user.name)
-	print("Bot id: %s" % bot.user.id)
-	print('------')
-	channel = discord.Object(id='token')
-	yield from bot.send_message(channel,'Ohayou!')
+    # event for when the bot is is done preparing the data received from discord
+    # prints out the name and id of the bot to the termainal
+    # and makes the bot send a ready message to the discord text channel
+    print('Login succesful')
+    print("Bot username: %s" % bot.user.name)
+    print("Bot id: %s" % bot.user.id)
+    print('------')
+    channel = discord.Object(id='channel_id')
+    yield from bot.send_message(channel,'Ohayou!')
 
 @bot.event
 @asyncio.coroutine
 def on_message(message):
 
-	# prevents the bot from responding to itself
-	if message.author == bot.user:
-		return
-	
-	if (message.content == 'ping'):
-		if (message.author.id == 'token'):
-			# BEEEEEES message
-			msg = '{0.author.mention} gnop!'.format(message)
-			yield from bot.send_message(message.channel, msg)
-		else:
-			# other people
-			msg = '{0.author.mention} pong!'.format(message)
-			yield from bot.send_message(message.channel, msg)
+    if message.author == bot.user:
+        # prevents the bot from responding to itself
+        return
 
+    if (message.content == 'ping'):
+        if (message.author.id == 'author_id'):
+            # BEEEEEES message
+            msg = '{0.author.mention} gnop!'.format(message)
+            yield from bot.send_message(message.channel, msg)
+        else:
+            # other people
+            msg = ('{0.author.mention} %s' % Random_Message()).format(message)
+            yield from bot.send_message(message.channel, msg)
 
-	# kill command
-	# it should only accept this command from my account
-	if (message.content == '!kill' and  message.author.id == 'token'):
-		yield from bot.send_message(message.channel, 'Oyasumi!')
-		yield from bot.close()
-		exit()
+    if "literally" in message.content:
+        # responds if a message contains a substring
+        yield from bot.send_message(message.channel, 'literally')
 
-# bot token, do not change this
-bot.run("token")
+    # admin commands
+    # commands in this section should only be executed if called by a specific user (hopefully the bot's owner)
+    if (message.author.id == 'admin_id'):
+        if (message.content == '!kill'):
+            # disconnects fromt the discord server and ends the script
+            yield from bot.send_message(message.channel, 'Oyasumi!')
+            yield from bot.close()
+            exit()
+
+def Random_Message():
+    # pulls a random response from a list and returns it as a string
+    # since randint uses the list length as the max value, added more repsonses involves simply adding strings to the list
+    # and restarting the script
+    r_response_list = ['reponse_1', 'reponse_2', 'reponse_3', 'response_4', 'reponse_5']
+    random = randint(0,(len(r_response_list)-1))
+    return r_response_list[random]
+
+def main():
+    # bot token, do not change this
+    # this allows the bot to login to the discord server
+    bot.run("bot_token")
+
+main()
